ff6b5cbd1400891718b8639a8971b697d6af7615
Jaco Koen <jacojkoen@gmail.com>
2024-12-10 13:21:32 +0100

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,30 +2,50 @@
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ["@nuxt/ui"],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
 
   imports: {
-    dirs: ['./constants/**', './types/**', './utils/**'],
+    dirs: ["./constants/**", "./types/**", "./utils/**"],
   },
 
   ui: {
-    icons: ['ion'],
+    icons: ["ion"],
   },
 
-  vite
-  : {
-      css
-  : {
-        preprocessorOptions
-  : {
-          scss
-  : {
-            additionalData
-  : '@use "~/assets/animation.scss" as *;'
-          }
-        }
-      }
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/animation.scss" as *;',
+        },
+      },
     },
+  },
 
-  compatibilityDate: '2024-10-05',
-})
+  compatibilityDate: "2024-10-05",
+});
