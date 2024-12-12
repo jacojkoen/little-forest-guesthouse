a893ccc730672aaaaefb94b73a6bf8b7894761cc
Jaco Koen <jacojkoen@gmail.com>
2024-12-12 13:02:05 +0100

    features as icons

--- components/lf/features-list.vue
+++ components/lf/features-list.vue
@@ -1,19 +1,22 @@
 <template>
-  <ul class="list-disc">
-    <li class="ml-4" v-for="feature in features">
-      <span>{{ feature }}</span>
+  <ul class="grid gap-x-4 gap-y-1 md:grid-cols-2">
+    <li class="ml-4 flex" v-for="feature in features">
+      <lf-icon :name="feature.icon" class="mr-2"></lf-icon>
+      <span>{{ feature.text }}</span>
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
 const features = [
-  'Ideal for short or long term stays whether for business, leisure, relocation, post surgery recovery or spiritual healing',
-  'Bed and Breakfast',
-  'Self Catering',
-  'Free high speed fibre optic Wi-Fi connection',
-  'On site transport',
-  'Secure off street parking',
-  'Laundry services',
-]
+  { text: "Paid Breakfast", icon: "i-ion-restaurant" },
+  { text: "Self Catering", icon: "i-ion-home" },
+  { text: "Free Wi-Fi", icon: "i-ion-wifi" },
+  { text: "Transport services", icon: "i-ion-bus" },
+  { text: "Free parking", icon: "i-ion-car" },
+  { text: "Outdoor pool (Pool Cottage)", icon: "i-ion-water" },
+  { text: "Pet Friendly", icon: "i-ion-paw" },
+  { text: "Laundry facilities", icon: "i-mdi-washing-machine" },
+  { text: "Housekeeping", icon: "i-mdi-broom" },
+];
 </script>
